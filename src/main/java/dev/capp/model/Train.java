743a72b84cd6824da136b1daba1f20febfffa706
Marcap00 <cappellatomarco@yahoo.com>
2025-01-25 14:37:46 +0100

    added getter and setter to Train class and override toString method

--- src/main/java/dev/capp/model/Train.java
+++ src/main/java/dev/capp/model/Train.java
@@ -1,4 +1,58 @@
 package dev.capp.model;
 
 public class Train {
+
+    private int trainId;
+    private String nameCompany;
+    private String trainCode;
+    private int year;
+
+    public Train (int trainId, String nameCompany, String trainCode, int year) {
+        this.trainId = trainId;
+        this.nameCompany = nameCompany;
+        this.trainCode = trainCode;
+        this.year = year;
+    }
+
+    public int getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(int trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Train{" +
+                "trainId=" + trainId +
+                ", nameCompany='" + nameCompany + '\'' +
+                ", trainCode='" + trainCode + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
